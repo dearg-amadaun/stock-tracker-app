8d9a95f69c411f66db81a9a4bcd213208697ec09
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-12-30 18:19:07 -0500

    building ticker calls in resources/stock.js

--- src/resources/stock.js
+++ src/resources/stock.js
@@ -2,11 +2,20 @@ import { iex }  from '../config/iex.js';
 
 export const stock = {
 
-    latestPrice: (ticker) => {
+    latestPrice: (ticker, callback) => {
         const url = `${iex.base_url}/stock/${ticker}/intraday-prices?chartLast=1&token=${iex.api_token}`
         
-        const query = fetch(url).then((response) => response.json())
-        return query;
+        fetch(url).
+        then((response) => response.json())
+        .then((data) => {
+            const stockData = data [data.length - 1]
+            const formattedData= {}
+            formattedData.price = stockData.close
+            formattedData.date = stockData.date
+            formattedData.time = stockData.label
+
+            callback(formattedData)
+        })
     }
 
 };
