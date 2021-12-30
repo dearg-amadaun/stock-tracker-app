bf2457b58f5bb443bc0d6a7c96148b5eb3f4a713
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-12-29 19:35:37 -0500

    component mount function

--- src/components/StockRow.js
+++ src/components/StockRow.js
@@ -5,19 +5,28 @@ class StockRow extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            price: 'price',
-            date: 'date',
-            time: 'time'
+            data: {}
         }
+    }
+
+    componentDidMount() {
+        //API query for Data
+        this.setState({
+            data: {
+                price: 'price',
+                date: 'date',
+                time: 'time'
+            }
+        })
     }
 
     render() {
         return (
             <tr>
                 <td>{this.props.ticker}</td>
-                <td>{this.state.price}</td>
-                <td>{this.state.date}</td>
-                <td>{this.state.time}</td>
+                <td>{this.state.data.price}</td>
+                <td>{this.state.data.date}</td>
+                <td>{this.state.data.time}</td>
             </tr>   
         )
     }
