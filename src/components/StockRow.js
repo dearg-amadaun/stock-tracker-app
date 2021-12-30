4890e7fb1641d64dd0388860185d903519e1b35d
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-12-29 19:32:04 -0500

    propr constructor and dummy data

--- src/components/StockRow.js
+++ src/components/StockRow.js
@@ -2,14 +2,23 @@ import React, { Component } from 'react';
 
 class StockRow extends Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            price: 'price',
+            date: 'date',
+            time: 'time'
+        }
+    }
+
     render() {
         return (
-            <tr className="">
+            <tr>
                 <td>{this.props.ticker}</td>
-                <td>{this.props.ticker}</td>
-                <td>{this.props.ticker}</td>
-                <td>{this.props.ticker}</td>
-            </tr>
+                <td>{this.state.price}</td>
+                <td>{this.state.date}</td>
+                <td>{this.state.time}</td>
+            </tr>   
         )
     }
 
