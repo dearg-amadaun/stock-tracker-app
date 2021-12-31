8487b7cf3d2ff5b512126b91a89a7b37958e2430
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-12-30 21:36:18 -0500

    rough component styling

--- src/App.js
+++ src/App.js
@@ -7,7 +7,17 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <table className="table mt-5">
+        <div className="col-md-5 mt-5">
+          <div className="card">
+              <ul className="list-group list-group-flush">
+                <StockRow ticker='AAPL' />
+                <StockRow ticker='GOOG' />
+                <StockRow ticker='GME' />
+                <StockRow ticker='AMC' />
+              </ul>
+          </div>
+        </div>
+        {/* <table className="table mt-5">
           <thead>
             <tr>
               <th>Ticker</th>
@@ -22,7 +32,7 @@ function App() {
               <StockRow ticker="GME" />
               <StockRow ticker="AMC" />
             </tbody>
-        </table>
+        </table> */}
       </div>
     </div>
   );
--- src/components/StockRow.js
+++ src/components/StockRow.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import { stock } from '../resources/stock.js';
 
+const changeStyle = {
+    color: '#4caf50',
+    fontSize: '0.8rem',
+    marginLeft: 5
+  }
+
 class StockRow extends Component {
 
     constructor(props) {
@@ -22,12 +28,12 @@ class StockRow extends Component {
 
     render() {
         return (
-            <tr>
-                <td>{this.props.ticker}</td>
-                <td>{this.state.data.price}</td>
-                <td>{this.state.data.date}</td>
-                <td>{this.state.data.time}</td>
-            </tr>   
+            <li className="list-group-item">
+                  <b>{this.props.ticker}</b> {this.state.data.price}
+                  <span className="change" style={changeStyle}>
+                    +13.34 (5.0%)
+                  </span>
+                </li>
         )
     }
 
