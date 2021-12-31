674fce7649b2775732c1c0fcd0da755a11a1f745
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-12-30 20:06:48 -0500

    cleaned up function in stock.js

--- src/resources/stock.js
+++ src/resources/stock.js
@@ -3,19 +3,22 @@ import { iex }  from '../config/iex.js';
 export const stock = {
 
     latestPrice: (ticker, callback) => {
-        const url = `${iex.base_url}/stock/${ticker}/intraday-prices?chartLast=1&token=${iex.api_token}`
-        
-        fetch(url).
-        then((response) => response.json())
-        .then((data) => {
-            const stockData = data [data.length - 1]
-            const formattedData= {}
-            formattedData.price = stockData.close
-            formattedData.date = stockData.date
-            formattedData.time = stockData.label
+        fetch(stock.latestPriceURL(ticker))
+        .then((response) => response.json())
+            .then((data) => callback(stock.formatPriceData(data)))
+    },
 
-            callback(formattedData)
-        })
+    latestPriceURL: (ticker) => {
+       return `${iex.base_url}/stock/${ticker}/intraday-prices?chartLast=1&token=${iex.api_token}`
+    },
+
+    formatPriceData: (data) => {
+        const stockData = data [data.length - 1]
+        const formattedData= {}
+        formattedData.price = stockData.close
+        formattedData.date = stockData.date
+        formattedData.time = stockData.label
+        return formattedData
     }
 
 };
