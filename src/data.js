8df5ba0ceebb893585515259eb96fb8edef92ee7
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2022-01-05 16:46:07 -0500

    dummy data

---
+++ src/data.js
@@ -0,0 +1,43 @@
+const data = {
+    exchanges: [
+        {
+            name: "DJI",
+            index: 841.42,
+            change: 1.54
+        },
+        {
+            name: "DJI",
+            index: 841.42,
+            change: 1.54
+        },
+        {
+            name: "DJI",
+            index: 841.42,
+            change: 1.54
+        },
+        {
+            name: "DJI",
+            index: 841.42,
+            change: 1.54
+        }
+    ],
+    stocks: [
+        {
+            name: "Google",
+            ticker: "GOOGL",
+            bid: 100.5,
+            ask: 135.8,
+            sector: "Industry",
+            dividends: {
+                2018: 18,
+                2017: 17,
+                2016: 16,
+                2015: 15
+            },
+            homepage: "https://www.google.com/",
+            investorpage: "https://www.google.com/"
+        }
+    ]
+}
+
+export default data;
